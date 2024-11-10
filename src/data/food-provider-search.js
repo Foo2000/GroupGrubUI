991a6c16f8e270d37b79c0894648ec48d2d191ae
Marian Abuhazi <marian.abuhazi@gmail.com>
2024-11-10 00:31:37 -0500

    some mock values for search bar

---
+++ src/data/food-provider-search.js
@@ -0,0 +1,7 @@
+export const restaurants = [
+	{ id: 1, name: "Milano's", location: "New York, NY" },
+	{ id: 2, name: "Shake Shack", location: "New York, NY" },
+	{ id: 3, name: "Din Tai Fung", location: "Seattle, WA" },
+	{ id: 4, name: "Taco Bell", location: "New York, NY" },
+];
+  
